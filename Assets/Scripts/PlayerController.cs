6c2f45f01a0a3970ae5d02c175ca1be5a13d046c
Brian Keegan <brian.morris.keegan@gmail.com>
2022-03-28 19:43:57 -0400

    Done with this shit!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,19 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    bool isOn = false;
     [SerializeField] float torqueAmount = 5f;
     [SerializeField] int forwardTorque = 50;
+    [SerializeField] int boostSpeed = 15;
+
+    SurfaceEffector2D surfaceEffector;
+
     Rigidbody2D rg2d;
     void Start()
     {
         rg2d = GetComponent<Rigidbody2D>();
+        surfaceEffector = FindObjectOfType<SurfaceEffector2D>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    private void RespondToBoost()
+    {
+        // throw new NotImplementedException();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (!isOn)
+            {
+                print("increase!");
+                surfaceEffector.speed += boostSpeed;
+            }
+            else
+            {
+                print("decrease!");
+                surfaceEffector.speed -= boostSpeed;
+            }
+            print(surfaceEffector.speed);
+            isOn = !isOn;
+        }
+    }
+
+    private void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
