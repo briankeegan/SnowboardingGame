4886555e701322007a47bf36283365e3752de4a1
Brian Keegan <brian.morris.keegan@gmail.com>
2022-03-28 22:40:24 -0400

    Add some sound

--- Assets/Scripts/Crash.cs
+++ Assets/Scripts/Crash.cs
@@ -5,16 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class Crash : MonoBehaviour
 {
+    bool hasPlayed = false;
+    private void PlaySoundOnce(AudioSource audioSource)
+    {
+        if (!hasPlayed)
+        {
+            audioSource.Play();
+            hasPlayed = true;
+        }
+    }
     [SerializeField] new ParticleSystem particleSystem;
+
+    AudioSource crashSound;
+
+    private void Start()
+    {
+        crashSound = GetComponent<AudioSource>();
+    }
     private void Restart()
     {
         // SceneManager.LoadScene(0);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        hasPlayed = false;
     }
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Ground")
         {
+
+            PlaySoundOnce(crashSound);
             particleSystem.Play();
             Debug.Log("Ouch!");
             // Add small delay
