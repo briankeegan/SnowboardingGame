45c6dfb678e7962501d4e3ea7b477f7cd48cb5f5
Brian Keegan <brian.morris.keegan@gmail.com>
2022-03-28 22:53:27 -0400

    add sound effects

--- Assets/Scripts/Lap.cs
+++ Assets/Scripts/Lap.cs
@@ -7,16 +7,18 @@ public class Lap : MonoBehaviour
 {
 
 
+    [SerializeField] AudioClip lapSound;
     [SerializeField] new ParticleSystem particleSystem;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Finish")
         {
+            AudioSource audioSource = GetComponent<AudioSource>();
+            audioSource.PlayOneShot(lapSound);
             particleSystem.Play();
             Debug.Log("Play is gonna loop around!");
             transform.position = new Vector3(0, 27, 0);
         }
     }
-    // This is bad practice, but fuck it, I hate this game. Its just for learning!
 }
